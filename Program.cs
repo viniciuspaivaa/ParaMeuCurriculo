f7d718865850278cf390b45007c4253f108d6282
Vinicius <vinicius.paiva2@outlook.com>
2024-10-01 11:17:10 -0300

    Criada classe Tarefa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Tarefa
+{
+    public string Nome {get; set;}
+    public bool Concluida {get; set;}
+
+    public Tarefa(string nome)
+    {
+        Nome = nome;
+        Concluida = false;
+    }
+
+    public void Exibir()
+    {
+        string status = Concluida ? "Concluída" : "Não concluída";
+        // if(Concluida)
+        // {
+        //     Console.Write("Concluída");
+        // }
+        // else
+        // {
+        //     Console.Write("Não concluída");
+        // }
+        Console.Write($"Tarefa: {Nome}\nStatus: {status}");
+    }
+}
